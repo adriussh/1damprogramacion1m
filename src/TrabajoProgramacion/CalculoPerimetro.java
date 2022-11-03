b4e867afa09be858be07f864c8bca4788da2ec9b
AdriánEscribanoS <adrian.escribanos@iesfm.onmicrosoft.com>
2022-11-03 12:30:56 +0100

    26/10

--- src/TrabajoProgramacion/CalculoPerimetro.java
+++ src/TrabajoProgramacion/CalculoPerimetro.java
@@ -1,4 +1,30 @@
 package TrabajoProgramacion;
 
+import java.util.Scanner;
+
 public class CalculoPerimetro {
-}
+        public static void main (String[] args) {
+
+            Scanner entrada = new Scanner(System.in);
+            int radio;
+            double perimetro, area, volumen;
+            double pi = 3.1416;
+            do {
+                System.out.println("Ingrese el radio del circulo a calcular: ");
+                radio = entrada.nextInt();
+                if (radio<0){
+                    System.out.println("El numero no puede ser negativo");
+                }
+            } while (radio <0);
+                perimetro = (int) (2 * pi * radio);
+                area = (int) (pi * radio * radio);
+                volumen = (int) (4 / 3 * pi * radio * radio * radio);
+                System.out.println("El perimetro es: " + perimetro + ".");
+                System.out.println("El area es: " + area + ".");
+                System.out.println("El volumen es: " + volumen + ".");
+
+
+
+        }
+    }
+
--- src/TrabajoProgramacion/Hipoteca.java
+++ src/TrabajoProgramacion/Hipoteca.java
@@ -4,56 +4,45 @@ import java.util.Scanner;
 
     public class Hipoteca {
         public static void main(String[] args) {
-            System.out.println("Vamos ha pedirte unos requisitos para saber si te podemos conceder o no una hipoteca, pulse 1 para comenzar");
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("¿Tienes trabajo? pulsa 1 si es si o pulsa 2 si es no");
             int trabajo = 0;
-            Scanner teclado = new Scanner(System.in);
-            trabajo=teclado.nextInt();
-            switch (trabajo) {
-                //Usamos case para introducir los requisitos ya que pueden haber dos casos, correctos y no correectos.
-                case 1: {
-                    float dinero_que_se_necesita = 0;
-                    float vivienda = 0;
-                    float division = 0;
-                    Scanner ds = new Scanner(System.in);
-                    System.out.println("Pon el dinero que deseas recibir como ayuda para la hipoteca");
-                    dinero_que_se_necesita = ds.nextFloat();
-                    //el Scanner ds lo hemos creado para que el dinero que la persona inserte se quede guardado como el dinero que se necesita para despues calcular la división.
+            do {
+                Scanner hipoteca = new Scanner(System.in);
+                trabajo = hipoteca.nextInt();
 
-                    Scanner vi = new Scanner(System.in);
-                    System.out.println("Introduzce ahora el precio que tiene la vivienda que ha elegido");
-                    vivienda = vi.nextFloat();
-                    //Este Scanner es igual que el anterior pero en este caso para que guarde el precio de la vivienda.
-                    division = dinero_que_se_necesita / vivienda;
-                    //El calculo del precio.
-                    System.out.println(division);
-                    if (division <= 0.8) {
-                        int dinero_ahorrado = 0;
-                        System.out.println("Introduce el dinero ahorrado que poseas");
-                        float division1 = 0;
-                        Scanner id = new Scanner(System.in);
+                switch (trabajo) {
+                    case 1:
+                        float vivienda =0;
+                        float division =0;
+                        float dinero_necesitado =0;
+                        Scanner vi = new Scanner(System.in);
+                        System.out.println("Introduce el dinero que necesita como ayuda");
+                        dinero_necesitado = vi.nextFloat();
+                        Scanner pr = new Scanner(System.in);
+                        System.out.println("Introduzca el valor de la vivienda");
+                        vivienda = pr.nextFloat();
+                        division = dinero_necesitado/vivienda;
+                        if (division<= 0.8){
+                            System.out.println("Introduzca el dinero que tiene ahorrado para la casa");
+                            Scanner ahorro = new Scanner(System.in);
+                            double dinero_que_tiene_ahorrado = 0;
+                            dinero_que_tiene_ahorrado= ahorro.nextDouble();
 
-
-                        division1 = dinero_ahorrado / vivienda;
-                        //si esta operación es correcta le saldra que si se le puede conceder ya que la división es mayor al 20%.
-
-                        System.out.println(+division1);
-                        if (division1 >= 0.2) {
-                            System.out.println("Se le puede conceder la hipoteca de la casa");
-                        } else if (division1 < 0.2) {
-                            //si la operación a  es menor del 20% nos saltara este mensaje diciendo que la hipoteca no sera concedida por la flata de dinero.
-
-                            System.out.println("No podemos concederte la hipoteca,lo sentimos");
-                            return;
-                            //el return finaliza con la ejecucion para que esta no siga hasta que alcanze el final y finalice.
+                            double division1 = 0;
+                            division1 = dinero_que_tiene_ahorrado/vivienda;
+                            if (division1>=0.2){
+                                System.out.println("Perfecto, tiene concedida la hipoteca de la casa :)");
+                            } else if (division1< 0.2) {
+                                System.out.println("La hipoteca no se le puede conceder");
+                            }
                         }
-                    }
-                }
-                case 2: {
-                    System.out.println("No podemos concederte la hipoteca,lo sientimos");
-                    //Este case lo ponemos para que cuando la division no sea la correcta salte este mensaje y termine el programa.
-                }
 
+
+                }
+            if (trabajo!=1){
+                System.out.println("No podemos concederle la hipoteca,lo sentimos.");
             }
-
+            } while(trabajo !=1);
         }
     }
